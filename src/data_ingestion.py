c52ca06ac2bc5a5c3b93d348e2bd0b35f87dc7ab
Syed Shahlal <sr.shahlal@gmail.com>
2024-03-15 15:46:31 -0400

    data ingestion module

---
+++ src/data_ingestion.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import streamlit as st
+
+@st.cache_data
+def data_ingestion():
+    # Load the dataset
+    url = "https://media.githubusercontent.com/media/syedshahlal/MDA_TextAnalysis/main/dataset/merged_compustat_and_labels.csv"
+    df = pd.read_csv(url)
+    return df
