4e96e4684b0dcb79619ee3e776816fb201c09f47
Syed Shahlal <sr.shahlal@gmail.com>
2024-03-26 02:49:58 -0400

    updated the class

--- src/dataset.py
+++ src/dataset.py
@@ -3,17 +3,23 @@ from torch.utils.data import Dataset, DataLoader, TensorDataset
 import torch
 
 class CSVDataset(Dataset):
-    """Dataset class for loading data from CSV files."""
+    #Constructor for initially loading
     def __init__(self, path):
         df = read_csv(path, header=0)
-        # Assume last column is target
-        self.X = df.iloc[:, :-1].values.astype('float32')
-        self.y = df.iloc[:, -1].values.astype('float32')
-    
+        self.X = df.values[0:, :-1]
+        self.y = df.values[0:, -1]
+        self.X = self.X.astype('float32')
+        self.y = self.y.astype('float32')
+        self.y = self.y.reshape((len(self.y), 1))
+
+        print(self.X.shape)
+        print(self.y.shape)
+
+    # Get the number of rows in the dataset
     def __len__(self):
         return len(self.X)
-    
-    def __getitem__(self, idx):
+    # Get a row at an index
+    def __getitem__(self,idx):
         return [self.X[idx], self.y[idx]]
 
 def prepare_train_dataset(path):
