481c0927fbba6c32c85b41a8ce9e77e26fdaf782
Syed Shahlal <sr.shahlal@gmail.com>
2024-04-17 10:11:26 -0400

    updated src .py

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,12 +1,15 @@
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
+import plotly.graph_objects as go
 from imblearn.under_sampling import RandomUnderSampler
 from imblearn.over_sampling import RandomOverSampler
+from collections import Counter
 import os
 import streamlit as st
 import torch
 from src.model import FraudDetectionMLP
+
 
 def model_exists(num_features, model_dir="model"):
     model_path = f"{model_dir}/fraud_detection_model_{num_features}_features.pth"
@@ -57,7 +60,7 @@ def data_preprocessing(df):
     return df
 
 @st.cache_data
-def split_data(df_filtered, cutoff_year):
+def split_data(df, train_period, test_period):
     """
     Split the dataset into training and testing sets based on a cutoff year.
     
@@ -70,38 +73,86 @@ def split_data(df_filtered, cutoff_year):
     - test_df: DataFrame containing the testing data.
     """
     # Split the DataFrame into training and testing based on the cutoff year
-    train_df = df_filtered[df_filtered['fyear'] <= cutoff_year]
-    test_df = df_filtered[df_filtered['fyear'] > cutoff_year]
-    
+    train_df = df[(df['fyear'] >= train_period[0]) & (df['fyear'] <= train_period[1])]
+    test_df = df[(df['fyear'] >= test_period[0]) & (df['fyear'] <= test_period[1])]
     return train_df, test_df
 
 
 @st.cache_data
-def data_resampling(df, cutoff_year, resampling_strategy):
+def data_resampling(df, train_period, test_period, resampling_strategy):
     """
-    Splits the data based on a cutoff year and applies the selected resampling strategy to balance the training set.
+    Splits the data based on selected training and testing periods and applies the selected 
+    resampling strategy to balance the training set.
+
+    Parameters:
+    - df: DataFrame containing the data.
+    - train_period: Tuple representing the start and end year of the training period.
+    - test_period: Tuple representing the start and end year of the testing period.
+    - resampling_strategy: String representing the resampling strategy ('RUS' or 'ROS').
+
+    Returns:
+    - X_train_resampled: Features of the resampled training set.
+    - y_train_resampled: Labels of the resampled training set.
+    - X_test: Features of the testing set.
+    - y_test: Labels of the testing set.
     """
     features = ['act', 'ap', 'at', 'ceq', 'che', 'cogs', 'csho', 'dlc', 'dltis', 'dltt', 'dp', 'ib',
                 'invt', 'ivao', 'ivst', 'lct', 'lt', 'ni', 'ppegt', 'pstk', 're', 'rect', 'sale', 'sstk',
                 'txp', 'txt', 'xint', 'prcc_f', 'dch_wc', 'ch_rsst', 'dch_rec', 'dch_inv', 'soft_assets',
                 'ch_cs', 'ch_cm', 'ch_roa', 'bm', 'dpi', 'reoa', 'EBIT', 'ch_fcf', 'issue']
-    
-    train_data = df[df['fyear'] <= cutoff_year]
-    test_data = df[df['fyear'] > cutoff_year]
+
+    # Update to use the train_period and test_period tuples
+    train_data = df[(df['fyear'] >= train_period[0]) & (df['fyear'] <= train_period[1])]
+    test_data = df[(df['fyear'] >= test_period[0]) & (df['fyear'] <= test_period[1])]
 
     X_train = train_data[features]
     y_train = train_data['misstate']
     X_test = test_data[features]
     y_test = test_data['misstate']
 
+    # Resampling strategy selection
     if resampling_strategy == 'Random Under Sampling (RUS)':
         sampler = RandomUnderSampler()
     elif resampling_strategy == 'Random Over Sampling (ROS)':
         sampler = RandomOverSampler()
 
+    # Apply resampling
     X_train_resampled, y_train_resampled = sampler.fit_resample(X_train, y_train)
 
+    counter_before = Counter(y_train)
+    counter_after = Counter(y_train_resampled)
+
+    # Convert to lists for Plotly
+    categories = list(counter_before.keys())
+    values_before = list(counter_before.values())
+    values_after = list(counter_after.values())
+
+    # Creating a figure with subplots
+    fig = go.Figure(data=[
+        go.Bar(name='Before Resampling', x=categories, y=values_before, text=values_before,
+            marker_color='red', textposition='auto'),
+        go.Bar(name='After Resampling', x=categories, y=values_after, text=values_after,
+            marker_color='blue', textposition='auto')
+    ])
+
+    # Update the layout
+    fig.update_layout(
+        barmode='group',
+        title='Class Distribution Before and After Resampling',
+        xaxis_title='Class',
+        yaxis_title='Frequency',
+        width=800,
+        height=450,
+    )
+
+    # Set x-axis category order if needed, assuming classes are 0 and 1
+    fig.update_xaxes(categoryorder='array', categoryarray=[0, 1])
+
+    # Show the figure
+    st.plotly_chart(fig)
+
     return X_train_resampled, y_train_resampled, X_test, y_test
+
 
 @st.cache_data
 def fin_ratio(X_train_resampled, y_train_resampled, X_test, y_test):
@@ -133,7 +184,7 @@ def fin_ratio(X_train_resampled, y_train_resampled, X_test, y_test):
     merged_train_data_14 = pd.concat([X_train_resampled_14, y_train_resampled_14], axis=1)
     merged_test_data_14 = pd.concat([X_test_14, y_test_14], axis=1)
 
-    directory = "./dataset"
+    directory = ".././dataset"
 
     if not os.path.exists(directory):
      os.makedirs(directory)
